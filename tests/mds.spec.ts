034bd4027be7216ce8a9db0db6aaaaa0e6bf5d7c
Saddam0070 <mdsansari0505@gmail.com>
2026-03-18 19:56:19 +0530

    file updated9

--- tests/mds.spec.ts
+++ tests/mds.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Google title check', async ({ page }) => {
+test('Google6 title check', async ({ page }) => {
   await page.goto('https://www.google.com');
   await expect(page).toHaveTitle(/Google/);
   console.log(await page.url());
