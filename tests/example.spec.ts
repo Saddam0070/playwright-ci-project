cd3488dc596e7304765b777bd13475afa0b625a2
Saddam0070 <mdsansari0505@gmail.com>
2026-03-18 16:00:11 +0530

    Google

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('Google title check', async ({ page }) => {
-  await page.goto('https://www.google.com');
+  await page.goto('https://ww.google.com');
   await expect(page).toHaveTitle(/Google/);
 });
