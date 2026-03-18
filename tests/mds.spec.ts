4d0f473617c3182265ce123ebef3982d7351f531
Saddam0070 <mdsansari0505@gmail.com>
2026-03-18 20:54:03 +0530

    simple04

--- tests/mds.spec.ts
+++ tests/mds.spec.ts
@@ -1,7 +1,22 @@
 import { test, expect } from '@playwright/test';
 
-test('Google36 title check', async ({ page }) => {
-  await page.goto('https://www.google.com');
-  await expect(page).toHaveTitle(/Google/);
-  console.log(await page.url());
+test('test', async ({ page }) => {
+  await page.goto('https://demowebshop.tricentis.com/register');
+  await page.getByLabel('Male', { exact: true }).check();
+  await page.getByLabel('First name:').click();
+  await page.getByLabel('First name:').fill('abc');
+  await page.getByLabel('Last name:').click();
+  await page.getByLabel('Last name:').fill('xyz');
+  await page.getByLabel('Email:').click();
+  //await page.getByLabel('Email:').fill('abc80987@gmail.com');
+    const randomEmail = `test${Math.floor(Math.random() * 10000)}@gmail.com`;
+
+await page.getByLabel('Email:').fill(randomEmail);
+
+  await page.getByLabel('Password:', { exact: true }).click();
+  await page.getByLabel('Password:', { exact: true }).fill('abc@123');
+  await page.getByLabel('Confirm password:').click();
+  await page.getByLabel('Confirm password:').fill('abc@123');
+  await page.getByRole('button', { name: 'Register' }).click();
+  await page.getByRole('button', { name: 'Continue' }).click();
 });
